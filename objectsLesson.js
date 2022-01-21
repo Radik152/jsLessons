372fd9e5c4acda2414c8141bd5dd9cb1a1fdbfa3
Radik152 <savio19992@gmail.com>
2022-01-21 23:14:35 +0300

    object tests

--- objectsLesson.js
+++ objectsLesson.js
@@ -13,3 +13,45 @@ user.surname = 'Smith';
 user.name = 'Pete';
 delete user.name;
 console.log(user);
+
+    // Проверка на пустоту (важность: 5)
+    // Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
+const isEmpty = (obj) => {
+    for(let key in obj) {
+        if(key) return false;
+    }
+    return true;
+}
+let schedule = {};
+console.log(isEmpty(schedule));
+schedule["8:30"] = "get up";
+console.log(isEmpty(schedule));
+
+    // Сумма свойств объекта (важность: 5)
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+let sum = 0;
+for( let key in salaries) {
+    sum += salaries[key];
+}
+console.log(sum);
+
+    // Умножаем все числовые свойства на 2 (важность: 3)
+    // Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
+const multiplyNumeric = (obj) => {
+    for(let key in obj) {
+        if(typeof obj[key] === 'number') {
+            obj[key]*= 2;
+        }
+    }
+}
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+multiplyNumeric(menu);
+console.log(menu);
